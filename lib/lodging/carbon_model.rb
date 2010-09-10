af8766272ecd302eeae1bcd53df40da5c6443832
Ian Hough <ian@brighterplanet.com>
2010-09-09 17:32:17 -0700

    Let's make this a characteristic of lodging, rather than lodging_class

--- lib/lodging/carbon_model.rb
+++ lib/lodging/carbon_model.rb
@@ -25,7 +25,7 @@ module BrighterPlanet
           
           committee :magnitude do # return room-nights - couldn't think of anything to call it except 'magnitude'
             quorum 'default' do
-              LodgingClass.fallback.magnitude
+              Lodging.fallback.magnitude
             end
           end
         end
