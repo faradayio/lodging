63b636ae9c2770fecc3fff5ab086b716bbbef38c
Ian Hough <ian@brighterplanet.com>
2010-09-09 17:04:52 -0700

    Add magnitude and emission factor

--- lib/lodging/carbon_model.rb
+++ lib/lodging/carbon_model.rb
@@ -7,9 +7,25 @@ module BrighterPlanet
       def self.included(base)
         base.extend ::Leap::Subject
         base.decide :emission, :with => :characteristics do
-          committee :emission do # returns kg CO2
+          committee :emission do # returns kg CO2e
+            quorum 'from magnitude and emission factor', :needs => [:magnitude, :emission_factor] do |characteristics|
+              characteristics[:magnitude] * characteristics[:emission_factor]
+            end
+            
             quorum 'default' do
-              1.0
+              raise "The emission committee's default quorum should never be called."
+            end
+          end
+          
+          committee :emission_factor do # returns kg CO2e per room night
+            quorum 'default' do
+              LodgingClass.fallback.emission_factor
+            end
+          end
+          
+          committee :magnitude do # return room-nights - couldn't think of anything to call it except 'magnitude'
+            quorum 'default' do
+              LodgingClass.fallback.magnitude
             end
           end
         end
