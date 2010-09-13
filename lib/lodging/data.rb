47e54a0d45cd44a5da32818fe51ab5449e1357c5
Ian Hough <ian@brighterplanet.com>
2010-09-13 16:25:07 -0700

    Holding off on this

--- lib/lodging/data.rb
+++ lib/lodging/data.rb
@@ -6,8 +6,6 @@ module BrighterPlanet
       def self.included(base)
         base.data_miner do
           schema do
-            string  'lodging_class_name'
-            integer 'magnitude'
           end
 
           process :run_data_miner_on_belongs_to_associations
