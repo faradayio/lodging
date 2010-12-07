a0fa62dfa4d1b092e700936866a06b9ea5c69a15
Seamus Abshere <seamus@abshere.net>
2010-12-07 11:27:48 -0600

    pull in fallback from production

--- lib/lodging/fallback.rb
+++ lib/lodging/fallback.rb
@@ -2,7 +2,8 @@ module BrighterPlanet
   module Lodging
     module Fallback
       def self.included(base)
-        base.falls_back_on
+        base.falls_back_on :rooms  => 1,
+                           :nights => 1
       end
     end
   end
