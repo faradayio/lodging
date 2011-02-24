501fb951ca005012266fa66601d7e3a97ba209cf
Ian Hough <ian@brighterplanet.com>
2011-02-23 18:04:04 -0800

    Add scope

--- lib/lodging.rb
+++ lib/lodging.rb
@@ -3,5 +3,6 @@ require 'emitter'
 module BrighterPlanet
   module Lodging
     extend BrighterPlanet::Emitter
+    scope 'The lodging emission estimate is the anthropogenic emissions from lodging room energy use. It includes CO2 emissions from direct fuel combustion and fuel combustion to generate electricity.'
   end
 end
