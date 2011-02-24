f2055675984f46286d85619cab496d8266203503
Ian Hough <ian@brighterplanet.com>
2011-02-24 10:00:27 -0800

    Tweak scope description

--- lib/lodging.rb
+++ lib/lodging.rb
@@ -3,6 +3,6 @@ require 'emitter'
 module BrighterPlanet
   module Lodging
     extend BrighterPlanet::Emitter
-    scope 'The lodging emission estimate is the anthropogenic emissions from lodging room energy use. It includes CO2 emissions from direct fuel combustion and fuel combustion to generate electricity.'
+    scope 'The lodging emission estimate is the anthropogenic emissions from lodging room energy use. It includes CO2 emissions from direct fuel combustion and indirect fuel combustion to generate purchased electricity.'
   end
 end
