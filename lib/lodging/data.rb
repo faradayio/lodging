98fcb2ec11255968921e8679bc38d10d430d4b96
Seamus Abshere <seamus@abshere.net>
2011-07-19 19:29:54 -0500

    create_table -> force_schema

--- lib/lodging/data.rb
+++ lib/lodging/data.rb
@@ -2,7 +2,7 @@ module BrighterPlanet
   module Lodging
     module Data
       def self.included(base)
-        base.create_table do
+        base.force_schema do
           string  'lodging_class_name'
           string  'zip_code_name'
           string  'state_postal_abbreviation'
