7f231ea6dd959b15ff005c50bb89c4b321e6b19f
Ian Hough <ijhough@gmail.com>
2012-05-01 18:13:28 +0200

    LodgingProperty is no longer in Earth so need to define here for tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,6 +12,9 @@ DataMiner.logger = Logger.new(nil)
 
 require 'sniff'
 
+# need to require this before initializing Sniff for LodgingProperty fixture to be loaded
+require File.expand_path('../lodging_property', __FILE__)
+
 Sniff.init File.join(File.dirname(__FILE__), '..', '..'),
   :adapter => 'mysql2',
   :database => 'test_lodging',
@@ -20,5 +23,3 @@ Sniff.init File.join(File.dirname(__FILE__), '..', '..'),
   :earth => [:hospitality, :fuel, :locality],
   :cucumber => true,
   :logger => 'log/test_log.txt'
-
-require File.expand_path('../lodging_property', __FILE__)
--- features/support/lodging_property.rb
+++ features/support/lodging_property.rb
@@ -1,5 +1,26 @@
-# Fallbacks for testing based on LodgingProperty as of 2012-02-21
-LodgingProperty.falls_back_on :fridge_coverage => 0.6,
-                              :hot_tubs        => 0.3,
-                              :pools_indoor    => 0.3,
-                              :pools_outdoor   => 0.6
+# LodgingProperty is defined in CM1 rather than Earth, so we need to define it here for testing
+class LodgingProperty < ActiveRecord::Base
+  col :northstar_id
+  col :name
+  col :city
+  col :locality # state / province / etc.
+  col :postcode # zip code / postal code / etc.
+  col :country_iso_3166_alpha_3_code
+  col :chain_name
+  col :lodging_rooms,     :type => :integer
+  col :floors,            :type => :integer
+  col :construction_year, :type => :integer
+  col :lodging_class_name
+  col :ac_coverage,       :type => :float
+  col :mini_bar_coverage, :type => :float
+  col :fridge_coverage,   :type => :float
+  col :hot_tubs,          :type => :float # float b/c fallback needs to be a float
+  col :pools_indoor,      :type => :float # float b/c fallback needs to be a float
+  col :pools_outdoor,     :type => :float # float b/c fallback needs to be a float
+  
+  # based on LodgingProperty as of 2012-02-21
+  falls_back_on :fridge_coverage => 0.6,
+                :hot_tubs        => 0.3,
+                :pools_indoor    => 0.3,
+                :pools_outdoor   => 0.6
+end
